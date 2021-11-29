c5913f9c430ece2af8abdda9642444d20e46ae8a
Jiali Yao <89944340+Jellyyyyy@users.noreply.github.com>
2021-11-29 15:02:34 -0500

    done with 05 extract data

--- data_pipeline/load_crime_testing-2.py
+++
@@ -1,5 +0,0 @@
-import json
-import bigquery as bq
-
-bq load --source_format NEWLINE_DELIMITED_JSON crimedata_vegas to_load.json
-ObjectId:int64,Event_Number,Event_Date,Type,Type_Description,General_Location,Beat,Disposition,Map_X,Map_Y,New_Y,LAT,LONG,WARD,geometry:GEOGRAPHY
--- data_pipeline/load_hospitaldata.py
+++ data_pipeline/load_hospitaldata.py
@@ -1,5 +1,9 @@
 from pipeline_tools import gcs_to_local_file, geopandas_to_gbq
 import geopandas as gpd
+import os
+
+credential_path = "final-509-409489d51842.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 def main(ds):
     local_path = gcs_to_local_file(
